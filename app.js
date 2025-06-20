fb23d0a041e0fe590a1a570eb3d11fd12098c4fe
Nnaji Maryann <64291754+amary-ann@users.noreply.github.com>
2025-06-20 12:40:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.querySelector(".validate-pin").addEventListener("click", async (e) => {
     e.preventDefault();
     const pin = document.getElementById("pin-input").value.trim();
+    const errorBox = document.getElementId("error");
     console.log("Pin entered:", pin);
     if (pin.length !== 6 || isNaN(pin)) {
         errorBox.classList.remove("hidden");
