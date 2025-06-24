7d98b891855dd85a54b245463c4e96edba58aa20
amary-ann <marynaj28@gmail.com>
2025-06-24 13:53:11 +0100

    changes to endpoint url

--- app.js
+++ app.js
@@ -5,26 +5,26 @@ document.querySelector(".validate-pin").addEventListener("click", async (e) => {
     console.log("Pin entered:", pin);
     if (pin.length !== 6 || isNaN(pin)) {
         errorBox.classList.remove("hidden");
-        errorBox.innerHTML += `<li style = "color: red;">Please enter a valid 6-digit PIN.</li>
+        errorBox.innerHTML += `<li style="color: red;">Please enter a valid 6-digit PIN.</li>
     `;
         return;
     }
     let pinData = {"pin": pin,"phone_number":"+2348183808266"};
     console.log(JSON.stringify(pinData));
-    pin_validation = await fetch("https://mimic-sparkle.onrender.com/submit-pin",
+    pin_validation = await fetch("https://mimic-sparkle.onrender.com/verify-transaction",
         { method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(pinData)
-        })
+        });
     pin_validation = await pin_validation.json();
     console.log(pin_validation);
 
-    if(pin_validation['status'] === "success") {
+    if(pin_validation['success'] == True) {
         document.getElementById("pin-input").value = "";
         errorBox.classList.remove("hidden");
-        errorBox.innerHTML += `<li style = "color: green;">Successful</li>`;
+        errorBox.innerHTML += `<li style = "color: green;">Your transaction has been processed</li>`;
     }
 
 });
