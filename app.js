d83162941a291ff914532fd45b7c453181714064
amary-ann <marynaj28@gmail.com>
2025-07-12 16:09:56 +0100

    commit

--- app.js
+++ app.js
@@ -2,14 +2,30 @@ document.querySelector(".validate-pin").addEventListener("click", async (e) => {
     e.preventDefault();
     const pin = document.getElementById("pin-input").value.trim();
     const errorBox = document.getElementById("error");
-    console.log("Pin entered:", pin);
+    const phoneNo = document.getElementById("phone-input").value.trim();
+
     if (pin.length !== 6 || isNaN(pin)) {
         errorBox.classList.remove("hidden");
         errorBox.innerHTML += `<li style="color: red;">Please enter a valid 6-digit PIN.</li>
     `;
         return;
     }
-    let pinData = {"pin": pin,"phone_number":"+2348183808266"};
+    // Normalize the number
+    if (phoneNo.startsWith("0")) {
+        phone = phoneNo.slice(1);
+    } else if (phoneNo.startsWith("234")) {
+        phone = phoneNo.slice(3);
+    } else if (phoneNo.startsWith("+234")) {
+        phone = phoneNo.slice(4);
+    } else {
+        phone = phoneNo;
+    }
+
+    // Always prepend +234
+    phoneNum = "+234" + phone;
+    console.log("Normalized phone number:", phoneNum);
+    
+    let pinData = {"pin": pin,"phone_number":phoneNum};
     console.log(JSON.stringify(pinData));
     pin_validation = await fetch("https://mimic-sparkle.onrender.com/verify-transaction",
         { method: "POST",
